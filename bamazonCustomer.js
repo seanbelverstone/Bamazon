593ae8401e7a00794e5d5cbb27af3afc8d5a0f95
Sean Belverstone <seanbelverstone@gmail.com>
2019-08-10 23:46:35 -0500

    Started fleshing out the start function, as it needs to display all the items in the bamazon database. Left comments for myself to continue tomorrow

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,4 +19,20 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected as user ID " + connection.threadId + "\n");
     start();
-})
+});
+
+function start() {
+    console.log("Welcome to \n ========== \n Bamazon \n ========== \n");
+    displayItems();
+}
+
+function displayItems() {
+    connection.query("SELECT * FROM products", function(err, results) {
+        if (err) throw err;
+        console.log("\nCurrent items in stock: \n")
+        results.forEach {
+            //For each to loop through the database array and print out in a nice, readable format
+
+        }
+    });
+}
