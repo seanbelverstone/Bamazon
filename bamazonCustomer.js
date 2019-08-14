c93d4e0d11f57db186ec6b3fede28941ebb0e1cb
Sean Belverstone <seanbelverstone@gmail.com>
2019-08-13 22:41:35 -0500

    Added the inquirer prompt. This includes validation and also displays the item that the user has selected, along with its price

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var results = [];
 
 var connection = mysql.createConnection({
 
@@ -36,14 +37,30 @@ function displayItems() {
             console.log("Price: " + results[i].price);
             console.log("Number in Stock: " + results[i].stock_quantity);
         }
-        inquirer.prompt ([
-
-            {
-                name: "selectID",
-                type: "list",
-                
-            }
-
-        ])
+    firstQuestion(results);
     });
+}
+
+function firstQuestion(results) {
+    inquirer.prompt ([
+
+        {
+            name: "selectID",
+            type: "input",
+            message: "Please type the ID of the number you wish to purchase:",
+            validate: function(value) {
+                var valid = isNaN(parseFloat(value));
+                if (valid === true) {
+                return "Please enter a valid ID number";
+                }
+            return true;
+            }
+        }
+
+    ]).then(function(response) {
+        //This stores the user input into a variable, and then minuses 1 so it can translate to array positions
+        var itemIndex = (response.selectID) -1;
+        console.log("\nYou have selected item " + results[itemIndex].item_id + ": " + results[itemIndex].product_name);
+        console.log("\nPrice: $" + results[itemIndex].price);
+    })
 }
